11bf9e3d642fd96edb7aff930ba16da6c1e2156f
kabutogani <a20dc119@dhw.ac.jp>
2023-04-13 16:08:51 +0900

    Config画面からVirtualCameraのPOV感度を設定できるように。

--- Assets/Scripts/UI/Config.cs
+++ Assets/Scripts/UI/Config.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using Cinemachine;
 
 
 public class Config : MonoBehaviour
@@ -9,21 +10,35 @@ public class Config : MonoBehaviour
     [SerializeField] private GameObject _configUI;
     private PlayerInputSet _playerInputSet;
 
+    private ICinemachineCamera icCam;
+
     // Start is called before the first frame update
     void Start()
     {
         _playerInputSet = PlayerInputSet.instance;
         _playerInputSet.Tab.Subscribe(x => {
             CameraMove.ChangePOVCursorMode(!x);
-            OnConfigUI(!x);
+            OnConfigUI(x);
         }).AddTo(this);
+
+
     }
 
     void OnConfigUI(bool i){
         _configUI.SetActive(i);
     }
 
-    void ChengeSensi(float i){
+    private void ChangeSensi(float value){
+        Debug.Log("ChangeTo" + value);
+        CinemachineBrain cinemachineBrain = Camera.main.gameObject.GetComponent<CinemachineBrain>();
+        icCam = cinemachineBrain.ActiveVirtualCamera;
+        CinemachineVirtualCamera vcam = icCam.VirtualCameraGameObject.GetComponent<CinemachineVirtualCamera>();
+        vcam.GetCinemachineComponent(CinemachineCore.Stage.Aim).GetComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = value * 5;
+        vcam.GetCinemachineComponent(CinemachineCore.Stage.Aim).GetComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed = value * 5;
         
+    }
+
+    public void Func1(float value){
+        ChangeSensi(value);
     }
 }

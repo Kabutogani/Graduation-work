f0352d344a0694c4c6ebb4099d5b890bc4492c12
kabutogani <a20dc119@dhw.ac.jp>
2023-04-26 13:23:25 +0900

    ドアの仮モデルを作成

---
+++ Assets/Scripts/Super/Intaractor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    private bool isInArea = false; 
+
+    void Start(){
+
+    }
+
+    void Update(){
+        if(isInArea){
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider collider){
+        isInArea = true;
+    }
+
+    void OnTriggerExit(Collider collider){
+        isInArea = false;
+    }
+}

8a64b8d15a75b11cf3f9042bfa179bab75624e77
kabutogani <a20dc119@dhw.ac.jp>
2023-05-11 14:17:33 +0900

    敵が動ける用のRouteを仮作成

---
+++ Assets/Scripts/Enemy/Path/EnemyRoute.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRoute : MonoBehaviour
+{
+    public List<EnemyRoute> AdjoiningRoute;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos(){
+        Gizmos.DrawIcon(gameObject.transform.position, "Route.png", false);
+    }
+
+    void OnDrawGizmosSelected() {
+        
+        Gizmos.color = Color.red;
+        if(AdjoiningRoute != null){
+            foreach(var i in AdjoiningRoute){
+                Gizmos.DrawLine(gameObject.transform.position, i.transform.position);
+            }
+        }
+	}
+}

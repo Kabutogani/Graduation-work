e1de709c9287aaedf6fb676a4ca0157c07a3c382
kabutogani <minecraft0920@outlook.com>
2023-09-20 18:18:22 +0900

    シーン切り替え処理を行うクラスを作成

---
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public static void LoadSceneSingle(string sceneName){
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public static void LoadSceneAdd(string sceneName){
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+    public static AsyncOperation LoadSceneSingleAsync(string sceneName, bool isActive){
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = isActive;
+
+        return scene;
+    }
+
+    public static void UnloadSceneAsync(string sceneName){
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+}

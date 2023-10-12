f3fbcfd78eba704d9ffe11197c56066a01a95b1b
kabutogani <minecraft0920@outlook.com>
2023-10-12 19:02:08 +0900

    TimeCounterPrefabの名前を変更

--- Assets/Scripts/SaveSystem/TimeCounter.cs
+++ Assets/Scripts/SaveSystem/TimeCounter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SaveLoader))]
 public class TimeCounter : MonoBehaviour,ILoadableSaveData
 {
     public static TimeCounter instance;

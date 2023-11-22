cd87a705c1b344c19a3607176f83139e1e499913
kabutogani <minecraft0920@outlook.com>
2023-11-22 18:42:38 +0900

    初回プレイ時のみに発生するダイアログを作成

--- Assets/Scripts/Gimmicks/PlayProgress/FirstPlay.cs
+++ Assets/Scripts/Gimmicks/PlayProgress/FirstPlay.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class FirstPlay : MonoBehaviour
 {
+    TextMessage textMessage;
+
     void Start()
     {
+        textMessage = gameObject.GetComponent<TextMessage>();
+
         if(!GameProgress.instance.IsPassedProgress(1)){
             Debug.Log("このデータでの初プレイです");
+            textMessage.DialogStart(0);
             GameProgress.instance.SetProgressNum(1);
         }else{
             Debug.Log("このデータは既プレイです");

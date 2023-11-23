ee616d117fce5fffb3504363a617be1e20d9fd7f
kabutogani <minecraft0920@outlook.com>
2023-11-23 17:13:18 +0900

    Room001の真ん中の本棚から最初の鍵が取得できるように

---
+++ Assets/Scripts/Gimmicks/StageGimmicks/Room001/KeyBookShelves.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMessage))]
+public class KeyBookShelves : MonoBehaviour, IInteractable,ILoadableSaveData
+{
+    private List<string> loadedDatas;
+    private List<string> currentDatas;
+    private TextMessage textMessage;
+    [SerializeField] bool isGotKey;
+    [SerializeField] GameObject itemObj;
+
+    public void ChangeDataValue(int localSaveNum, string data)
+    {
+        SaveLoader saveLoader = this.gameObject.GetComponent<SaveLoader>();
+        saveLoader.tempDatas[localSaveNum] = data;
+    }
+
+    public void DataLoad(List<string> datas)
+    {
+        loadedDatas = datas;
+        currentDatas = loadedDatas;
+        if(datas[0] != null && datas[0] != ""){
+            isGotKey = false;
+        }else{
+            SetDefault();
+        }
+    }
+
+    public void OnInteract()
+    {
+        textMessage = GetComponent<TextMessage>();
+        if(isGotKey){
+            textMessage.DialogStart(1);
+        }else{
+            textMessage.DialogStart(0);
+            ChangeDataValue(0, true.ToString());
+            isGotKey = true;
+            GameProgress.instance.SetProgressNum(2);
+            Inventory.instance.AddItemForInventory("最初の鍵",itemObj);
+        }
+    }
+
+    public void SetDefault()
+    {
+        isGotKey = false;
+    }
+}

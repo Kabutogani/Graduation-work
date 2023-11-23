d6220f289f1b08fb18664f4d8364c478089c25b2
kabutogani <minecraft0920@outlook.com>
2023-11-23 18:16:12 +0900

    LockedDoorをSaveDataを利用するように修正

--- Assets/Scripts/Gimmicks/StageGimmicks/Doors/LockedDoor.cs
+++ Assets/Scripts/Gimmicks/StageGimmicks/Doors/LockedDoor.cs
@@ -2,15 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LockedDoor : Door
+[RequireComponent(typeof(TextMessage))]
+public class LockedDoor : Door,ILoadableSaveData
 {
+    private List<string> loadedDatas;
+    private List<string> currentDatas;
     [SerializeField] string _keyName;
+    [SerializeField] bool isLocked;
+    TextMessage textMessage;
+
+    public void ChangeDataValue(int localSaveNum, string data)
+    {
+        SaveLoader saveLoader = this.gameObject.GetComponent<SaveLoader>();
+        saveLoader.tempDatas[localSaveNum] = data;
+    }
+
+    public void DataLoad(List<string> datas)
+    {
+        loadedDatas = datas;
+        currentDatas = loadedDatas;
+        if(datas[0] != null && datas[0] != ""){
+
+            isLocked = bool.Parse(datas[0]);
+
+        }else{
+            SetDefault();
+        }
+    }
 
     public override void OnInteract(){
-        if(ItemChecker.CheckItemOnInventory(_keyName)){
-            OpenAndClose(!isActive);
+        textMessage = GetComponent<TextMessage>();
+
+        if(isLocked){
+            if(ItemChecker.CheckItemOnInventory(_keyName)){
+                ChangeDataValue(0, "false");
+                isLocked = false;
+                textMessage.DialogStart(1);
+            }else{
+                textMessage.DialogStart(0);
+            }
         }else{
-            Debug.Log("YouHaveNotKey!!");
+            OpenAndClose(!isActive);
         }
+    }
+
+    public void SetDefault()
+    {
+        isLocked = true;
     }
 }

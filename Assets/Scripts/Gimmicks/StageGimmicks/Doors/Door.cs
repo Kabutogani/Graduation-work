e928472f35ce0e4957168784bb86e39da93ce545
kabutogani <minecraft0920@outlook.com>
2023-11-23 17:47:53 +0900

    通常のドアがセーブデータを参照しないように変更

--- Assets/Scripts/Gimmicks/StageGimmicks/Doors/Door.cs
+++ Assets/Scripts/Gimmicks/StageGimmicks/Doors/Door.cs
@@ -3,30 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour, IInteractable, ILoadableSaveData
+public class Door : MonoBehaviour, IInteractable
 {
     [SerializeField]
-    protected GameObject door;
+    protected GameObject[] doors;
     [SerializeField]protected bool isActive;
-    private List<string> loadedDatas;
-    private List<string> currentDatas;
-
-    public void ChangeDataValue(int localSaveNum, string data)
-    {
-        SaveLoader saveLoader = this.gameObject.GetComponent<SaveLoader>();
-        saveLoader.tempDatas[localSaveNum] = data;
-    }
-
-    public void DataLoad(List<string> datas)
-    {
-        loadedDatas = datas;
-        currentDatas = loadedDatas;
-        if(datas[0] != null && datas[0] != ""){
-            OpenAndClose(bool.Parse(datas[0]));
-        }else{
-            SetDefault();
-        }
-    }
 
     public virtual void OnInteract()
     {   
@@ -36,12 +17,11 @@ public class Door : MonoBehaviour, IInteractable, ILoadableSaveData
     }
 
     protected void OpenAndClose(bool isOpen){
-        door.SetActive(isOpen);
+        foreach (var item in doors)
+        {
+            item.SetActive(isOpen);
+        }
         isActive = isOpen;
-        ChangeDataValue(0, isOpen.ToString());
     }
 
-    public virtual void SetDefault(){
-        OpenAndClose(true);
-    }
 }

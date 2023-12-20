9bcbd7dded1de213819542f9d09c2728e0c9fe8f
kabutogani <minecraft0920@outlook.com>
2023-12-20 16:07:51 +0900

    セーブした際にセーブポイントごとに設定された番号の場所を保存するように

--- Assets/Scripts/Gimmicks/StageGimmicks/SavePoint/SavePoint.cs
+++ Assets/Scripts/Gimmicks/StageGimmicks/SavePoint/SavePoint.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(TextMessage))]
-public class SavePoint : MonoBehaviour, IInteractable
+public class SavePoint : MonoBehaviour, IInteractable, ILoadableSaveData
 {
     TextMessage textMessage;
+    [SerializeField]GameObject loadPlayerSetPoint;
+    public int SavePointNum;
+    private int loadedSavePointNum;
     public void OnInteract()
     {
         textMessage = GetComponent<TextMessage>();
@@ -14,7 +17,33 @@ public class SavePoint : MonoBehaviour, IInteractable
     }
 
     public void Save(){
+        ChangeDataValue(0,SavePointNum.ToString());
         SaveSystem.Save();
         Debug.Log("セーブしました");
+    }
+
+    public void DataLoad(List<string> datas)
+    {
+        if(datas[0] != null && datas[0] != ""){
+            loadedSavePointNum = int.Parse(datas[0]);
+            if(loadedSavePointNum == SavePointNum){
+                PlayerStateMgr.instance.gameObject.transform.position = loadPlayerSetPoint.transform.position;
+            }
+        }else{
+            SetDefault();
+        }
+    }
+
+    public void ChangeDataValue(int localSaveNum, string data)
+    {
+        SaveLoader saveLoader = this.gameObject.GetComponent<SaveLoader>();
+        int i = int.Parse(data);
+        string tmpData = i.ToString();
+        saveLoader.tempDatas[localSaveNum] = tmpData;
+    }
+
+    public void SetDefault()
+    {
+        
     }
 }
